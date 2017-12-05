b90ea9420cbb134dc254dd04337b0c7e2d023747
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-12-05 11:37:11 -0600

    034 refine solution

--- 034_digit_factorials.js
+++ 034_digit_factorials.js
@@ -1,12 +1,12 @@
 const digitFactorials = () => {
   const factorials = [1];
+  let result = 0;
   for (let i=1; i<10; i++) factorials[i] = factorials[i - 1] * i;
-  const results = [];
   for (let i=3; i<factorials[9]; i++) {
     let factSum = i.toString().split('').reduce((sum, num) => sum + factorials[num], 0);
-    if (factSum === i) results.push(i);
+    if (factSum === i) result += i;
   }
-  return results.reduce((sum, num) => sum + num);
+  return result;
 };
 
 console.log(digitFactorials());
